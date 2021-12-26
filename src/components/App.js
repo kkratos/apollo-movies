3f5e106eb575bfba3e7ab100f8371f28afd0e150
kamal patel <kamal.patel30@gmail.com>
2021-12-26 17:53:35 +0530

    #0.3 Router and Styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Detail from '../routes/Detail';
+import Home from '../routes/Home';
+
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/:id" element={<Detail />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/routes/Detail.js
@@ -0,0 +1,6 @@
+const Detail = () => {
+    return (
+        <h1>Detail</h1>
+    )
+}
+export default Detail;
