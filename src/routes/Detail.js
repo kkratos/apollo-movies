e352505c289fc57efd6b7540c821b8df420fb36d
kamal patel <kamal.patel30@gmail.com>
2021-12-27 14:28:17 +0530

    #1.2 GET_MOVIES Query

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,33 @@
+import { useParams } from "react-router-dom";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+
 const Detail = () => {
-    return (
-        <h1>Detail</h1>
-    )
+    const { id } = useParams()
+    const { loading, data } = useQuery(GET_MOVIE, {
+        variables: { id: +id }
+    });
+    console.log(loading, data);
+    // console.log(id);
+    if (loading) {
+        return "loading"
+    }
+    if (data && data.movie) {
+        return data.movie.title;
+    }
+    
+    return <h1>Detail</h1>
+
 }
 export default Detail;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,81 @@
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+import styled from "styled-components";
+import Movie from "../components/Movie";
+
+const GET_MOVIES = gql`
+    {
+        movies{
+            id
+            medium_cover_image
+        }
+    }
+`
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+`
+
+const Header = styled.header`
+    background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+    height: 45vh;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+`
+
+const Title = styled.h1`
+    font-size: 60px;
+    font-weight: 600;
+    margin-bottom: 20px;
+`
+
+const Subtitle = styled.h3`
+    font-size: 35px;
+`
+
+const Loading = styled.div`
+    font-size: 18px;
+    opacity: 0.5;
+    font-weight: 500;
+    margin-top: 10px;
+`
+
+const Movies = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 25px;
+    position: relative;
+    top: -50px;
+`
+
 const Home = () => {
+    const { loading, data } = useQuery(GET_MOVIES);
     return (
-        <h1>Home</h1>
+        <Container>
+            <Header>
+                <Title>Apollo Movies</Title>
+                <Subtitle>I Love GraphQL</Subtitle>
+            </Header>
+            {loading && <Loading>Loading...</Loading>}
+            {!loading && data.movies && data.movies.map(m => <Movie key={m.id} id={m.id} />)}
+            {/* <Movies>
+                {data?.movies?.map(m => (
+                    <Movie>
+                        key={m.id}
+                        id={m.id}
+                        isLiked={m.isLiked}
+                        bg={m.medium_cover_image}
+                    </Movie>
+                ))}
+            </Movies> */}
+        </Container>
     )
 }
 
